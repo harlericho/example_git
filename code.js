fd1431060702bb932cb07dc33bcf2f67360abef6
Karlo Xavier Chok <harlericho@gmail.com>
2025-01-31 22:00:50 -0500

    fix: modificando el index.html y agregando boton de alerta

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
-window.addEventListener("load", () => {
+// window.addEventListener("load", () => {
+//   alert("aprendiendo git con ramas master y develop");
+// });
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
   alert("aprendiendo git con ramas master y develop");
 });
