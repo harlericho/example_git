6019be7793e11c61742f9cc6812a0b29566d7e16
Visualtuto86 <visualtutoriales86@gmail.com>
2025-05-20 20:07:25 -0500

    modificaciones rama_example5

--- code.js
+++ code.js
@@ -5,3 +5,8 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   alert("aprendiendo git con ramas master y develop");
 });
+// click al boton info
+const infoButton = document.querySelector("#info");
+infoButton.addEventListener("click", () => {
+  alert("aprendiendo git con ramas master y develop");
+});
