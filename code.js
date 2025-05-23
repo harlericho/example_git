95c23d234e8b0f73509b1f233ad7cfa74d127b12
harlericho <harlericho@gmail.com>
2025-05-23 16:14:49 -0500

    agregacion de notificaciones

--- code.js
+++ code.js
@@ -1,6 +1,4 @@
-// window.addEventListener("load", () => {
-//   alert("aprendiendo git con ramas master y develop");
-// });
+$.notify("Hello World", "success");
 const button = document.querySelector("#click");
 button.addEventListener("click", () => {
   alert("aprendiendo git con ramas master y develop");
@@ -15,13 +13,3 @@ const detailsButton = document.querySelector("#details");
 detailsButton.addEventListener("click", () => {
   alert("Este ejercicio esta realizado por Karlo Xavier Chok");
 });
-// click en el menu git del navbar
-// const gitButton = document.querySelector("#git_link");
-// gitButton.addEventListener("click", () => {
-//   alert("Navbar de git");
-// });
-// click en el menu details del navbar
-// const detailsButton2 = document.querySelector("#details_link");
-// detailsButton2.addEventListener("click", () => {
-//   alert("Navbar de detalles");
-// });
